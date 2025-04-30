20b1c23d223994398500f2dff14876341c93e318
ErenYeagerX-prog <insanecriminal54@gmail.com>
2025-04-30 09:25:04 +0530

    Update alive.py

--- AvishaRobot/modules/alive.py
+++ AvishaRobot/modules/alive.py
@@ -11,24 +11,24 @@ from telethon import __version__ as tver
 from AvishaRobot import SUPPORT_CHAT, pbot,BOT_USERNAME, OWNER_ID,BOT_NAME,START_IMG
 
 MISHI = [
-    "https://graph.org/file/f86b71018196c5cfe7344.jpg",
-    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
-    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
-    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
-    "https://graph.org/file/84e84ff778b045879d24f.jpg",
-    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
-    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
-    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg"
-    "https://graph.org/file/37248e7bdff70c662a702.jpg",
-    "https://graph.org/file/0bfe29d15e918917d1305.jpg",
-    "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
-    "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
-    "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
-    "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
-    "https://graph.org/file/39d7277189360d2c85b62.jpg",
-    "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
-    "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
-    "https://graph.org/file/3514efaabe774e4f181f2.jpg",
+    "https://files.catbox.moe/djfoag.jpg",
+    "https://files.catbox.moe/qmbt8e.jpg",
+    "https://files.catbox.moe/9xkmt4.jpg",
+    "https://files.catbox.moe/6z3ql9.jpg",
+    "https://files.catbox.moe/we0wkq.jpg",
+    "https://files.catbox.moe/7ce0py.jpg",
+    "https://files.catbox.moe/az2wq5.jpg",
+    "https://files.catbox.moe/3doasu.jpg"
+    "https://files.catbox.moe/az2wq5.jpg",
+    "https://files.catbox.moe/we0wkq.jpg",
+    "https://files.catbox.moe/6z3ql9.jpg",
+    "https://files.catbox.moe/7ce0py.jpg",
+    "https://files.catbox.moe/az2wq5.jpg",
+    "https://files.catbox.moe/3doasu.jpg",
+    "https://files.catbox.moe/6z3ql9.jpg",
+    "https://files.catbox.moe/we0wkq.jpg",
+    "https://files.catbox.moe/6z3ql9.jpg",
+    "https://files.catbox.moe/we0wkq.jpg",
 ]
 
 Avisha = [
