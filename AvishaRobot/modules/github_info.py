eee3bce6f181fa39a8fc74a1fc0ad506e842b5cc
ErenYeagerX-prog <insanecriminal54@gmail.com>
2025-04-30 10:00:33 +0530

    Update github_info.py

--- AvishaRobot/modules/github_info.py
+++ AvishaRobot/modules/github_info.py
@@ -46,7 +46,7 @@ async def github(_, message):
 ● ғᴏʟʟᴏᴡᴇʀs ➥ {followers}
 ● ғᴏʟʟᴏᴡɪɴɢ ➥ {following}
 
-❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [๛ᴀ ʟ ɪ s ʜ ᴀ ࿐](https://t.me/AlisaMusicRobot)"""
+❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [๛Eren࿐](https://t.me/ErenYeager_RoxBot)"""
             except Exception as e:
                 await message.reply(f"#ERROR {e}")
                   
